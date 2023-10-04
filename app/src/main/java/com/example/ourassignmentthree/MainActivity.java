0a0cfcd5ee27cd053cb37a0e1291666cd34a1b1c
jp378 <jpoutoa865@gmail.com>
2023-10-04 18:28:27 +1300

    Test Commit

--- app/src/main/java/com/example/ourassignmentthree/MainActivity.java
+++ app/src/main/java/com/example/ourassignmentthree/MainActivity.java
@@ -19,13 +19,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
     public void onClickStart(View view){
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
--- app/src/main/java/com/example/ourassignmentthree/MapsActivity.java
+++ app/src/main/java/com/example/ourassignmentthree/MapsActivity.java
@@ -29,7 +29,6 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
     private final int FINE_PERMISSION_CODE = 1;
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
@@ -37,59 +36,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected double longitude;
     FusedLocationProviderClient fusedLocationProviderCLient;
     Location currentLocation;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-
-        //if(mMap == null){
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-            mapFragment.getMapAsync(MapsActivity.this);
-        //}
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapsActivity.this);
         checkPermissionsGranted();
-        //fusedLocationProviderCLient = LocationServices.getFusedLocationProviderClient(this);
-
     }
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         getLastKnownLocation();
-        if(currentLocation != null){
-            latitude = currentLocation.getLatitude();
-            longitude = currentLocation.getLongitude();
-
-            LatLng myLocation = new LatLng(latitude, longitude);
-
-//            //Set the marker to be the orange drawable
-//            BitmapDescriptor customMarker = BitmapDescriptorFactory.fromResource(R.drawable.img_mm_marker_orange);
-//            //Add marker to the current location and name it "Current Location"
-//            mMap.addMarker(new MarkerOptions().position(myLocation).icon(customMarker).title("My Current Location"));
-//            //Move the camera to where the current location is
-//            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 15));
-
-        }
     }
-
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        if(requestCode == FINE_PERMISSION_CODE){
-//            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-//                //getLastKnownLocation();
-//            }
-//            else{
-//                Toast.makeText(this, "Location permission is denied, please allow the permission", Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//    }
-
     public void checkPermissionsGranted() {
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 || ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -100,7 +60,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             permissionGranted();
         }
     }
-
     public void askForPermission() {
         ActivityResultLauncher<String[]> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
             boolean fineGranted = result.getOrDefault(android.Manifest.permission.ACCESS_FINE_LOCATION, false);
@@ -117,22 +76,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 permissionNotGranted();
             }
         });
-
         activityResultLauncher.launch(new String[]{
                 android.Manifest.permission.ACCESS_COARSE_LOCATION,
                 android.Manifest.permission.ACCESS_FINE_LOCATION
         });
     }
-
     public void permissionGranted() {
         Toast.makeText(this, "LOCATION ACCESS GRANTED", Toast.LENGTH_SHORT).show();
         getLastKnownLocation();
     }
-
     public void permissionNotGranted() {
         Toast.makeText(this, "LOCATION ACCESS NOT GRANTED", Toast.LENGTH_SHORT).show();
     }
-
     public void getLastKnownLocation() {
         fusedLocationProviderCLient = (FusedLocationProviderClient) LocationServices.getFusedLocationProviderClient(this);
         //Ask for location
@@ -146,10 +101,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onSuccess(Location location) {
                 if(location != null){
                     currentLocation = location;
-
                     Log.d("LocationDebug", "Latitude: " + currentLocation.getLatitude() + ", Longitude: " + currentLocation.getLongitude());
-
-                    //Initialize the map only if it hasn't been initialized yet
                     LatLng newLat = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
                     showMarker(newLat, "Current location");
                 }
@@ -159,7 +111,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
     }
-
     public void showMarker(LatLng location, String markerTitle){
         //Set the marker to be the orange drawable
         BitmapDescriptor customMarker = BitmapDescriptorFactory.fromResource(R.drawable.img_mm_marker_orange);
