2deb40bc514a5630a0e72d21745a0f02211a1892
William <William@DESKTOP-UAAQGI8>
2023-10-08 19:11:23 +1300

    Update to file structure - added a web camera activity - added and started editing xml for new activity_web_camera_detail.xml
    Need to:
    - Implement an onclick to get to new activity
    - Implement a way to view live footage from api

---
+++ app/src/main/java/com/example/ourassignmentthree/WebCameraDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.ourassignmentthree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WebCameraDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_camera_detail);
+    }
+}
